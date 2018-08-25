1f3d31ac28ebac0a85347c8ec7832aa6ace95ad7
Valentin Slavov <vnslavov@gmail.com>
2018-08-25 22:52:56 +0300

    Animate four triangles creating a square

--- Main.elm
+++ Main.elm
@@ -2,7 +2,7 @@ module Main exposing (main)
 
 import Browser
 import Color.Palette exposing (darkYellow, purple)
-import Html exposing (Html, div, h1)
+import Html exposing (Html, div, h1, span)
 import Html.Attributes as Attr
 import Html.Events exposing (onClick)
 import Svg exposing (..)
@@ -22,7 +22,10 @@ main =
 
 type alias Model =
     { styles : List Animation.State
-    , tria : Animation.State
+    , tria1 : Animation.State
+    , tria2 : Animation.State
+    , tria3 : Animation.State
+    , tria4 : Animation.State
     }
 
 
@@ -30,7 +33,10 @@ init : () -> ( Model, Cmd Msg )
 init _ =
     ( { styles =
             List.map Animation.style polygons
-      , tria = Animation.style <| tria 300 400 darkYellow
+      , tria1 = Animation.style <| tria 500 200 darkYellow
+      , tria2 = Animation.style <| tria 500 200 darkYellow
+      , tria3 = Animation.style <| tria 500 200 darkYellow
+      , tria4 = Animation.style <| tria 500 200 darkYellow
       }
     , Cmd.none
     )
@@ -38,7 +44,7 @@ init _ =
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
-    Animation.subscription Animate [ model.tria ]
+    Animation.subscription Animate [ model.tria1, model.tria2, model.tria3, model.tria4 ]
 
 
 type Msg
@@ -52,10 +58,26 @@ update msg model =
     case msg of
         MorphTria ->
             let
-                newStyle =
-                    tria 200 500 purple
+                newTria2Style =
+                    [ Animation.points [ ( 500, 0 ), ( 700, 0 ), ( 700, 500 ) ]
+                    , Animation.fill purple
+                    ]
+
+                newTria3Style =
+                    [ Animation.points [ ( 700, 500 ), ( 700, 700 ), ( 200, 700 ) ]
+                    , Animation.fill darkYellow
+                    ]
+
+                newTria4Style =
+                    [ Animation.points [ ( 200, 700 ), ( 0, 700 ), ( 0, 200 ) ]
+                    , Animation.fill purple
+                    ]
             in
-                ( { model | tria = Animation.interrupt [ Animation.to newStyle ] model.tria }
+                ( { model
+                    | tria2 = Animation.interrupt [ Animation.to newTria2Style ] model.tria2
+                    , tria3 = Animation.interrupt [ Animation.to newTria3Style ] model.tria3
+                    , tria4 = Animation.interrupt [ Animation.to newTria4Style ] model.tria4
+                  }
                 , Cmd.none
                 )
 
@@ -75,7 +97,12 @@ update msg model =
             )
 
         Animate time ->
-            ( { model | tria = Animation.update time model.tria }
+            ( { model
+                | tria1 = Animation.update time model.tria1
+                , tria2 = Animation.update time model.tria2
+                , tria3 = Animation.update time model.tria3
+                , tria4 = Animation.update time model.tria4
+              }
             , Cmd.none
             )
 
@@ -132,13 +159,14 @@ polygons2 =
 view : Model -> Html Msg
 view model =
     div
-        [ onClick MorphTria
-        , Attr.style "margin" "200px auto"
+        [ Attr.style "margin" "200px auto"
         , Attr.style "width" "700px"
         , Attr.style "height" "750px"
-        , Attr.style "cursor" "pointer"
         ]
-        [ h1 [] [ text "Click to begin!" ]
+        [ h1 [ Attr.style "cursor" "pointer" ]
+            [ span [ onClick MorphTria, Attr.style "margin-right" "30px" ] [ text "Step 1" ]
+            , span [ onClick ChangeColors ] [ text "Step 2" ]
+            ]
         , mainCanvas model
         ]
 
@@ -147,7 +175,10 @@ mainCanvas : Model -> Html.Html msg
 mainCanvas model =
     svg
         [ version "1.1", width "700", height "750", viewBox "0 0 700 750" ]
-        [ polygon (Animation.render model.tria) []
+        [ polygon (Animation.render model.tria1) []
+        , polygon (Animation.render model.tria2) []
+        , polygon (Animation.render model.tria3) []
+        , polygon (Animation.render model.tria4) []
 
         -- [ g [] (List.map (\poly -> polygon (Animation.render poly) []) model.styles)
         ]
