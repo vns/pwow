5417f0daad6655f1d3a9230e8d0612ef9cc7e8f0
Valentin Slavov <vnslavov@gmail.com>
2018-08-25 21:47:24 +0300

    Create simple triangle morphing animation

--- Main.elm
+++ Main.elm
@@ -8,6 +8,7 @@ import Html.Events exposing (onClick)
 import Svg exposing (..)
 import Svg.Attributes exposing (..)
 import Animation
+import Debug
 
 
 main =
@@ -21,6 +22,7 @@ main =
 
 type alias Model =
     { styles : List Animation.State
+    , tria : Animation.State
     }
 
 
@@ -28,6 +30,7 @@ init : () -> ( Model, Cmd Msg )
 init _ =
     ( { styles =
             List.map Animation.style polygons
+      , tria = Animation.style <| tria 300 400 darkYellow
       }
     , Cmd.none
     )
@@ -35,20 +38,53 @@ init _ =
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
-    Animation.subscription Animate model.styles
+    Animation.subscription Animate [ model.tria ]
 
 
 type Msg
     = Animate Animation.Msg
+    | ChangeColors
+    | MorphTria
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        Animate time ->
-            ( { model | styles = List.map (Animation.update time) model.styles }
+        MorphTria ->
+            let
+                newStyle =
+                    tria 200 500 purple
+            in
+                ( { model | tria = Animation.interrupt [ Animation.to newStyle ] model.tria }
+                , Cmd.none
+                )
+
+        ChangeColors ->
+            ( { model
+                | styles =
+                    List.map2
+                        (\style newStyle ->
+                            Animation.interrupt
+                                [ Animation.to newStyle ]
+                                style
+                        )
+                        model.styles
+                        polygons2
+              }
             , Cmd.none
             )
+
+        Animate time ->
+            ( { model | tria = Animation.update time model.tria }
+            , Cmd.none
+            )
+
+
+tria : Float -> Float -> Color.Palette.Color -> List Animation.Property
+tria base height color =
+    [ Animation.points [ ( 0, 0 ), ( base, 0 ), ( 0, height ) ]
+    , Animation.fill color
+    ]
 
 
 polygons : List (List Animation.Property)
@@ -72,10 +108,32 @@ polygons =
     ]
 
 
+polygons2 : List (List Animation.Property)
+polygons2 =
+    [ [ Animation.points
+            [ ( 0, 0 ), ( 400, 0 ), ( 0, 300 ) ]
+      , Animation.fill purple
+      ]
+    , [ Animation.points
+            [ ( 400, 0 ), ( 700, 0 ), ( 700, 400 ) ]
+      , Animation.fill darkYellow
+      ]
+    , [ Animation.points
+            [ ( 700, 400 ), ( 700, 700 ), ( 300, 700 ) ]
+      , Animation.fill darkYellow
+      ]
+    , [ Animation.points
+            [ ( 300, 700 ), ( 0, 700 ), ( 0, 300 ) ]
+      , Animation.fill purple
+      ]
+    ]
+
+
 view : Model -> Html Msg
 view model =
     div
-        [ Attr.style "margin" "200px auto"
+        [ onClick MorphTria
+        , Attr.style "margin" "200px auto"
         , Attr.style "width" "700px"
         , Attr.style "height" "750px"
         , Attr.style "cursor" "pointer"
@@ -89,7 +147,9 @@ mainCanvas : Model -> Html.Html msg
 mainCanvas model =
     svg
         [ version "1.1", width "700", height "750", viewBox "0 0 700 750" ]
-        [ g [] (List.map (\poly -> polygon (Animation.render poly) []) model.styles)
+        [ polygon (Animation.render model.tria) []
+
+        -- [ g [] (List.map (\poly -> polygon (Animation.render poly) []) model.styles)
         ]
 
 
