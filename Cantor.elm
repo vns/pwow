651628e109ae1e95682335a09cd0d6d7e6e67a90
Valentin Slavov <vnslavov@gmail.com>
2018-09-30 13:05:25 +0300

    Add cantor set implemented with elm-collage

---
+++ Cantor.elm
@@ -0,0 +1,61 @@
+module Cantor exposing (main)
+
+import Browser
+import Html
+import Html.Attributes
+import Collage exposing (..)
+import Collage.Layout exposing (..)
+import Collage.Render exposing (svg)
+import Color exposing (..)
+
+
+main =
+    Browser.sandbox
+        { init = \_ -> {}
+        , update = \_ msg model -> {}
+        , view = view
+        }
+
+
+gap : Collage msg
+gap =
+    spacer 20 10
+
+
+hline : Color -> Float -> Collage msg
+hline color len =
+    rectangle len 10 |> filled (uniform Color.blue)
+
+
+cantor : Float -> Int -> Collage msg
+cantor len depth =
+    case depth of
+        0 ->
+            hline black len
+
+        _ ->
+            let
+                aLine =
+                    cantor (len / 3) (depth - 1)
+            in
+                [ aLine
+                , spacer (len / 3) 0
+                , aLine
+                ]
+                    |> horizontal
+                    |> center
+
+
+view model =
+    let
+        cantorRow =
+            \n ->
+                [ gap
+                , cantor 1200 n
+                ]
+    in
+        List.range 0 6
+            |> List.map cantorRow
+            |> List.concat
+            |> vertical
+            |> svg
